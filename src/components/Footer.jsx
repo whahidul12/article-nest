6d68ad50209150c51dc247437a88577566bd9d48
whahidul12 <mdislamwhahid@gmail.com>
2026-01-18 20:19:43 +0600

    feat: Revamp Footer component with new logo, links, and updated description; remove proxy middleware

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,22 @@
+import Link from "next/link";
+import logo from "../../public/logo.jpg";
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <section className="py-10 bg-gray-50 sm:pt-16 lg:pt-24">
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
           <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
-            <img
-              className="w-auto h-9"
-              src="https://cdn.rareblocks.xyz/collection/celebration/images/logo.svg"
-              alt=""
-            />
+            <Link href="/" className="flex items-center">
+              <Image className="w-auto h-8" src={logo} alt="Logo" />
+              <span className="text-black ml-2 text-2xl">Article Nest</span>
+            </Link>
 
             <p className="text-base leading-relaxed text-gray-600 mt-7">
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
-              Velit officia consequat duis enim velit mollit.
+              A curated article platform delivering clear, honest, and impactful writing.
+              No clickbait—just meaningful content that helps you think better and grow
+              faster.
             </p>
 
             <ul className="flex items-center space-x-3 mt-9">
@@ -94,52 +98,41 @@ export default function Footer() {
 
           <div>
             <p className="text-sm font-semibold tracking-widest text-gray-400 uppercase">
-              Company
+              Quick Links
             </p>
 
             <ul className="mt-6 space-y-4">
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/"
                   title=""
                   className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
                 >
                   {" "}
-                  About{" "}
-                </a>
+                  Home{" "}
+                </Link>
               </li>
 
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/articles"
                   title=""
                   className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
                 >
                   {" "}
-                  Features{" "}
-                </a>
+                  Articles{" "}
+                </Link>
               </li>
 
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/add-articles"
                   title=""
                   className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
                 >
                   {" "}
-                  Works{" "}
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="#"
-                  title=""
-                  className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
-                >
-                  {" "}
-                  Career{" "}
-                </a>
+                  Add Articles{" "}
+                </Link>
               </li>
             </ul>
           </div>
@@ -176,7 +169,7 @@ export default function Footer() {
         <hr className="mt-16 mb-10 border-gray-200" />
 
         <p className="text-sm text-center text-gray-600">
-          © Copyright 2021, All Rights Reserved by Postcraft
+          © Copyright 2026, All Rights Reserved by Article Nest
         </p>
       </div>
     </section>
