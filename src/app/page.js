9787bc8a5eef22dbbaa4f2a81ea390d8e7accdd8
whahidul12 <mdislamwhahid@gmail.com>
2026-01-18 23:58:40 +0600

    feat: Add new components and enhance the homepage layout
    - Integrated FAQ, HowItWorks, Stats, and Testimonial components into the homepage.
    - Updated Feature component to use camelCase for SVG attributes for consistency.
    - Implemented FAQ functionality with collapsible questions and answers.
    - Created HowItWorks section to explain the process in three steps.
    - Developed Stats section to showcase business metrics.
    - Added Testimonial section to display client feedback with star ratings.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 import Aurora from "@/components/backgrounds/Aurora";
+import Faq from "@/components/FAQ/Faq";
 import Feature from "@/components/feature/Feature";
 import Hero from "@/components/hero/Hero";
+import HowItWorks from "@/components/howItWorks/HowItWorks";
+import Stats from "@/components/stats/Stats";
+import Testimonial from "@/components/testimonial/Testimonial";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <HowItWorks />
       <Feature />
+      <Testimonial />
+      <Stats />
+      <Faq />
     </>
   );
 }
